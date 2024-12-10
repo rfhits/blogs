0290a94f331ab5c530d463146e9a00e04d028dc7
rfhits <wysj0117@gmail.com>
2024-12-10 12:57:58 +0800

    fix: use constexpr

--- languages/c-cpp/cpp-template/template-fibo.cpp
+++ languages/c-cpp/cpp-template/template-fibo.cpp
@@ -1,44 +1,24 @@
-template <int x>
-int fib()
-{
-    return fib<x - 1>() + fib<x - 2>();
-}
+template <int x> constexpr int fib() { return fib<x - 1>() + fib<x - 2>(); }
 
-template <>
-int fib<0>()
-{
-    return 1;
-}
+template <> constexpr int fib<0>() { return 1; }
 
-template <>
-int fib<1>()
-{
-    return 1;
-}
+template <> constexpr int fib<1>() { return 1; }
 
-template <int x>
-struct fib_t
-{
+template <int x> struct fib_t {
     static int const value = fib_t<x - 1>::value + fib_t<x - 2>::value;
 };
 
-template <>
-struct fib_t<0>
-{
+template <> struct fib_t<0> {
     static int const value = 1;
 };
 
-template <>
-struct fib_t<1>
-{
+template <> struct fib_t<1> {
     static int const value = 1;
 };
 
 #include <iostream>
 
-int
-main()
-{
+int main() {
     std::cout << fib<5>() << std::endl;
     std::cout << fib_t<6>::value << std::endl;
     return 0;
